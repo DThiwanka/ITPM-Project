ce5dc960eab389c96d7735f94be8751864aaf0e3
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2022-03-17 12:58:48 +0530

    Home Modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactLoading from "react-loading";
 
+import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ITPM PROJECT</h1>
+      <div className="anima">
+        <ReactLoading
+            type="spinningBubbles"
+            color="#0000FF"
+            height={200}
+            width={100}
+        />
+      </div>
     </div>
   );
 }
