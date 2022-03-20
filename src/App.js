36719ff819c3ef79e592b4e76cf0b86016d09cda
Kavisha Silva <it20126292@my.sliit.lk>
2022-03-20 11:16:00 +0530

    updates-commit_2022/03/20

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
+import home from './components/home'
+import check from './components/checkout/checkout'
+import Footer from './components/footer/footer'
+import Header from './components/header/header'
+import placed from './components/placed/placed'
 
-function App() {
+export default class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter key={App.BrowserRouter}>
+      <Header/>
+   <div className="container">
+     <Route path="/" exact component={home} key={App.home}></Route>
+     <Route path="/check" exact component={check} key={App.check}></Route>
+     <Route path="/placed" exact component={placed} key={App.placed}></Route>
+    
+  </div>
+  <Footer/>
+  </BrowserRouter>
+
   );
 }
 
-export default App;
+}
