f1142d5ce307e3fc38fa17bd78169c7f9f6ec107
Kavisha Silva <it20126292@my.sliit.lk>
2022-04-12 02:39:28 +0530

    add .gitignore

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import './bootstrap.min.css';
 import './index.css';
 import App from './App';
 
@@ -12,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
