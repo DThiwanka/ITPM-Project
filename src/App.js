a18c7c731a1d1ef18464638787da2beb59628c0e
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2022-04-16 10:10:42 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from './Components/Header'
 import Footer from './Components/Footer'
-import Home from './Components/Home'
+import Home from './Components/home'
 
 const App = () => {
   return (
