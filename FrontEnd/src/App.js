87f1bbb453967b5b04bc76a226b9ee2b08a3aecd
Kavisha Silva <it20126292@my.sliit.lk>
2022-05-06 00:20:52 +0530

    update_project-files

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,31 +1,12 @@
-import React, { Component } from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
-import home from './components/home'
-import check from './components/checkout/checkout'
-import Footer from './components/footer/footer'
-import Header from './components/header/header'
-import placed from './components/placed/placed'
-import orderdetails from './components/orderDetails/orderdetails'
-import login from './components/login/login'
-import register from './components/register/register'
+import React from 'react';
 
-export default class App extends Component {
-  render() {
+
+function App() {
   return (
-    <BrowserRouter key={App.BrowserRouter}>
-      <Header/>
-   <div className="container">
-     <Route path="/" exact component={home} key={App.home}></Route>
-     <Route path="/check" exact component={check} key={App.check}></Route>
-     <Route path="/placed" exact component={placed} key={App.placed}></Route>
-     <Route path="/orderdetails" exact component={orderdetails} key={App.orderdetails}></Route>
-     <Route path="/login" exact component={login} key={App.login}></Route>
-     <Route path="/register" exact component={register} key={App.register}></Route>
-  </div>
-  <Footer/>
-  </BrowserRouter>
-
+    <div className="container">
+      <h1>Welcome To City Medicals</h1>
+    </div>
   );
 }
 
-}
+export default App;
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 
 
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
