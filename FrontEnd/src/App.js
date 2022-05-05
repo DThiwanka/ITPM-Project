1711cc2c44d79f73cade7dcd6c8b001fd0228122
Kavisha Silva <it20126292@my.sliit.lk>
2022-05-06 00:46:04 +0530

    css_update

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
-    <div className="container">
+    <>
+    <Header/>
+    <main>
       <h1>Welcome To City Medicals</h1>
-    </div>
+    </main>
+    <Footer/>
+    </>
   );
 }
 
