ce54a823f3e69b96a0ed6f71f36948e5456ad052
Dulaj Thiwanka <86193439+DThiwanka@users.noreply.github.com>
2022-05-16 23:13:44 +0530

    admin prt 1

---
+++ FrontEnd/src/components/admin/admin.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function admin() {
+    return (
+        <div>
+            <div className="container">
+                <center>
+                <h1>Admin</h1>
+                </center>
+                
+            </div>
+        </div>
+    )
+}
+
+export default admin
--- FrontEnd/src/components/placed/placed.js
+++ FrontEnd/src/components/placed/placed.js
@@ -4,27 +4,39 @@ import './placed.css'
 
 function placed() {
   return (
-    <div>
+    <div class="d-md-flex h-md-100">
 
-<div className="flex-container">
+    <div class="col-md-6 p-0 bg-indigo h-md-100">
+        <div class="text-white d-md-flex h-100 p-5">
+            <div class="logoarea pt-5 pb-5">
 
-<div className="flex-child magenta">
-<div className="hclass   mt-3">
+
+            <div className="svg d-flex justify-content-center">
+			<div className="hclass mt-3">
       <h1>Order <d> Placed </d>!</h1>
       <p>Order will recived as soon as possible<br/>
       within two or three working days.
       </p>
       <p style={{color: '#2949ff'}}>Thanks for deaing with us :)</p>
       </div>
-<Deliverysvg
-    height={500}
-    width={500}
-    />
-</div>
+              
+            </div>
+            <Deliverysvg
+                height={500}
+                width={500}
+              />
 
-<div className="flex-child green">
-<div className="card bg-light shadow p-3 mb-10 bg-white rounded mt-5">
-  <div className="card-body">
+            </div>
+        </div>
+    </div>
+
+     
+    <div class="col-md-6 p-0 bg-white loginarea">
+        <div class="d-md-flex align-items-center h-md-100 p-5 justify-content-center">
+
+
+        <div className="card bg-light shadow bg-white rounded mt-5" style={{border: '18rem;'}}>
+        <div className="card-body">
     <h5 className="card-title">CITY MEDICALS</h5>
     <h6 className="card-subtitle mb-2 text-muted">Online Payment Receipt</h6>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -39,13 +51,14 @@ function placed() {
     <button href="/" className="card-link btn btn-danger">Another link</button>
     <button href="/" className="card-link btn btn-primary">Another link</button>
   </div>
-</div>
-</div>
+  </div>
 
-</div>
-
+            
+        </div>
     </div>
-  )
+        
+</div>
+)
 }
 
 export default placed
